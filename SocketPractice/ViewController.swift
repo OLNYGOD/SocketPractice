581cb0dc2a3689fb3ad0ed1aefef2bb04dae82b8
10.168.3.171\bill.w.chen <Fubon68023>
2025-11-26 10:40:11 +0800

    Initial Commit

---
+++ SocketPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketPractice
+//
+//  Created by bill.w.chen on 2025/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
